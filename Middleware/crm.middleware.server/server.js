02dd10d1552243be995968d9f19c97197f1ffa5d
elena360plus <elena.kachur@360plus.ca>
2019-04-25 13:21:49 -0700

    Commit

---
+++ Middleware/crm.middleware.server/server.js
@@ -0,0 +1,24 @@
+const http = require("http");
+const express = require("express");
+const bodyParser = require('body-parser');
+ 
+const app = express();
+
+app
+    .use( bodyParser.urlencoded({ extended: false }) )
+    .use( bodyParser.json({limit: '50mb', type: 'application/json'}) )
+    // .use( express.static(path.join(__dirname, 'www/build')) )
+    .use("/api/v1", require("./routes/api/v1") );
+
+
+// Handle React routing, return all requests to React app
+app.get('*', function(req, res) 
+{
+    res.sendFile(path.join(__dirname, 'www/build', 'index.html'));
+});
+
+
+const port = process.env.PORT ? process.env.PORT :  9999;
+
+const httpServer = http.createServer(app);
+httpServer.listen(port, ()=> console.log("server is running on port: %s", port) );
