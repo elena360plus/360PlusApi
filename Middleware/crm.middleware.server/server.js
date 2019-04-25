efa50fa1bd583bddf0a1e34fc394018428c1fe27
elena360plus <elena.kachur@360plus.ca>
2019-04-24 23:36:46 -0700

    server update

--- Middleware/crm.middleware.server/server.js
+++ Middleware/crm.middleware.server/server.js
@@ -8,15 +8,12 @@ app
     .use( bodyParser.urlencoded({ extended: false }) )
     .use( bodyParser.json({limit: '50mb', type: 'application/json'}) )
     // .use( express.static(path.join(__dirname, 'www/build')) )
-
     .use("/api/v1", require("./routes/api/v1") );
-    // .use("/resources", require("./routes/resources") );
 
 
 // Handle React routing, return all requests to React app
 app.get('*', function(req, res) 
 {
-    // console.log("get: ", req.baseUrl);
     res.sendFile(path.join(__dirname, 'www/build', 'index.html'));
 });
 
@@ -24,7 +21,4 @@ app.get('*', function(req, res)
 const port = process.env.PORT ? process.env.PORT :  9999;
 
 const httpServer = http.createServer(app);
-httpServer.listen(port, ()=>
-{
-    console.log("server is running on port: %s", port);
-});
+httpServer.listen(port, ()=> console.log("server is running on port: %s", port) );
