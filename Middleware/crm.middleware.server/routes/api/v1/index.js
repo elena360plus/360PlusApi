c7d233fd7363b0fcb29b8e651acac5f5f2ac73af
jenyagh <deynega@gmail.com>
2019-05-05 18:50:43 -0700

    crm routing

--- Middleware/crm.middleware.server/routes/api/v1/index.js
+++ Middleware/crm.middleware.server/routes/api/v1/index.js
@@ -1,10 +1,15 @@
 const apiV1 = require("express").Router();
 
-
 apiV1.get("/", async (req, res)=>
 {
     res.send("Hello from CRM-Middleware");
 });
+
+
+apiV1.use("/crm", require("./crm"));
+
+
+
 
 
 
--- Middleware/crm.middleware.server/server.js
+++ Middleware/crm.middleware.server/server.js
@@ -1,6 +1,22 @@
-const http = require("http");
 const express = require("express");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
+
+// const fetch = require("node-fetch");
+
+require("dotenv").config();
+
+// const domain = "360plus.dev";
+// const username = "Administrator";
+// const password = "User12345";
+// "http://win-p7or6uki9ss/PlusDev/api/data/v8.2/quotes"
+// const url = `http://win-p7or6uki9ss/PlusDev/api/data/v8.2/quotes`;
+
+// { CrmService, CrmResponse, CrmConnectionConfig, CrmO365ConnectionConfig, CrmAdConnectionConfig }
+// http://blog.yagasoft.com/2018/09/connect-dynamics-crm-node
+// https://docs.microsoft.com/en-us/dynamics365/customer-engagement/web-api/accountleads?view=dynamics-ce-odata-9
+
+
+// await crmService.initialise();
  
 const app = express();
 
@@ -17,8 +33,5 @@ app.get('*', function(req, res)
     res.sendFile(path.join(__dirname, 'www/build', 'index.html'));
 });
 
-
 const port = process.env.PORT ? process.env.PORT :  9999;
-
-const httpServer = http.createServer(app);
-httpServer.listen(port, ()=> console.log("server is running on port: %s", port) );
+app.listen(port, ()=> console.log("CRM Middleware Server is running on port: %s", port) );
