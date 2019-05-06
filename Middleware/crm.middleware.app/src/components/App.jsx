e87ecb645d078ee52fb815df959a7f60ae8b9005
jenyagh <deynega@gmail.com>
2019-05-05 22:43:40 -0700

    react response

--- Middleware/crm.middleware.app/src/components/App.jsx
+++ Middleware/crm.middleware.app/src/components/App.jsx
@@ -1,19 +1,57 @@
 import React from 'react';
+import Loading from "./Loading/Loading";
 import './App.css';
+
+
+class api
+{
+	campaign = async (id)=>
+	{
+		try
+		{
+			const campaign = await fetch(`/api/v1/crm/campaigns/${id}`);
+			const data = await campaign.json();
+			return data;
+	
+		}
+		catch(err)
+		{
+			console.log(err);
+		}
+	}
+	
+}
+
+
 
 class App extends React.Component
 {
+	server = new api();
+	
+	constructor(props)
+	{
+		super(props);
+		
+		// const {email, campaign} = props.params && props.params;
+		this.state = {params: props.params};
+	}
+
+
+	async componentWillMount()
+	{
+		const campaign = await this.server.campaign(this.state.campaign);
+		this.setState({campaign: campaign});
+	}
+
 	render()
 	{
-		return (
-			<div className="App">
-				<header className="App-header">
-					360 Plus IT
-				</header>
-				<img src="/img/crm.progress.png" />
-			</div>
-		);
+		console.log("campaign: ", this.state.campaign);
+
+		if ( ! this.state.campaign )
+			return ( <Loading />);
+
+		return (<div>We've received your response</div>)
 	}
 }
-
+// ?campaign=id&email=email@asdasd.com
 export default App;
--- Middleware/crm.middleware.app/src/index.js
+++ Middleware/crm.middleware.app/src/index.js
@@ -4,7 +4,34 @@ import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+function getParams()
+{
+    let initialParams = {};
+    let searchParams = new URLSearchParams(window.location.search);
+    
+    initialParams.email = searchParams.get("email");
+    initialParams.campaign = searchParams.get("campaign");
+    initialParams.response = searchParams.get("response");
+    
+    if ( initialParams.campaign )
+    {        
+        searchParams.delete("email");
+        searchParams.delete("campaign");
+        searchParams.delete("response");
+        if (window.history.replaceState) 
+        {
+            let params = searchParams.toString();
+            if ( params.length > 0 ) params = `?${params}`;
+            window.history.replaceState({}, null, `${window.location.origin}${params}`);                
+        }        
+    }
+    
+    return initialParams;
+}
+
+
+ReactDOM.render(<App params={getParams()}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
